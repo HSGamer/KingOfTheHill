1bba5cda5a9521534a9b4be6c24734dde45cc2cb
HSGamer <huynhqtienvtag@gmail.com>
2022-01-29 20:09:01 +0700

    display-value

--- src/main/java/me/hsgamer/kingofthehill/config/MainConfig.java
+++ src/main/java/me/hsgamer/kingofthehill/config/MainConfig.java
@@ -9,7 +9,7 @@ import org.bukkit.plugin.Plugin;
 public class MainConfig extends PathableConfig {
     public static final IntegerConfigPath MAX_TOP_DISPLAY = new IntegerConfigPath("max-top-display", 10);
     public static final StringConfigPath NULL_TOP_DISPLAY_NAME = new StringConfigPath("null-top-display-name", "---");
-    public static final StringConfigPath NULL_TOP_DISPLAY_VALUE = new StringConfigPath("null-top-display-name", "---");
+    public static final StringConfigPath NULL_TOP_DISPLAY_VALUE = new StringConfigPath("null-top-display-value", "---");
 
     public MainConfig(Plugin plugin) {
         super(new BukkitConfig(plugin, "config.yml"));
