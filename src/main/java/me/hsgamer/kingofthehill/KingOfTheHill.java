e618b36c7240dd8ae9597e09ab08ecce47602fa5
HSGamer <huynhqtienvtag@gmail.com>
2023-01-03 18:52:22 +0700

    remove hologram

--- src/main/java/me/hsgamer/kingofthehill/KingOfTheHill.java
+++ src/main/java/me/hsgamer/kingofthehill/KingOfTheHill.java
@@ -1,6 +1,5 @@
 package me.hsgamer.kingofthehill;
 
-import com.gmail.filoghost.holographicdisplays.api.HologramsAPI;
 import me.clip.placeholderapi.expansion.PlaceholderExpansion;
 import me.hsgamer.hscore.bukkit.baseplugin.BasePlugin;
 import me.hsgamer.hscore.bukkit.utils.MessageUtils;
@@ -83,20 +82,6 @@ public final class KingOfTheHill extends BasePlugin {
             };
             expansion.register();
             disableList.add(expansion::unregister);
-        }
-        if (Bukkit.getPluginManager().isPluginEnabled("HolographicDisplays")) {
-            arenaManager.getAllArenas().forEach(arena -> {
-                String pluginName = getName().toLowerCase(Locale.ROOT);
-                String name = arena.getName();
-                HologramsAPI.registerPlaceholder(this, "{" + pluginName + "_time_" + name + "}", 1, () -> arenaManager.getArenaCooldown(name));
-                HologramsAPI.registerPlaceholder(this, "{" + pluginName + "_state_" + name + "}", 1, () -> arenaManager.getArenaState(name));
-                for (int i = 0; i < MainConfig.MAX_TOP_DISPLAY.getValue(); i++) {
-                    String selector = name + ":" + i;
-                    HologramsAPI.registerPlaceholder(this, "{" + pluginName + "_top_name_" + selector + "}", 1, () -> arenaManager.getTopName(selector));
-                    HologramsAPI.registerPlaceholder(this, "{" + pluginName + "_top_value_" + selector + "}", 1, () -> arenaManager.getTopValue(selector));
-                }
-            });
-            disableList.add(() -> HologramsAPI.unregisterPlaceholders(this));
         }
     }
 
